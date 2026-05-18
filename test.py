3658f42515ab362cc7509b19843f1ef412c7f4a6
Shatakshi Tiwari <shatakshi.tiwari@siemens.com>
2026-05-18 13:57:20 +0530

    check custom

---
+++ test.py
@@ -0,0 +1,18 @@
+# // SPDX-License-Identifier: LicenseRef-MyCustom
+# // Copyright 2026 Test Author
+# // This file tests custom license handling.
+
+# testing for turtle built direct.
+# added expanded licensing.
+# test for see also
+# validator added for all the formats 
+# official validator added
+# imports added
+# redundant calls for 2 versions removed 
+# validation parsing done 
+# validation fixes
+# check: scanner name updated 
+# rdf check
+# enum added
+# format fixes
+# licenseText tested 
